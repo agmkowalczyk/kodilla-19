67262f3fcd76264d3bfe7851a27f9e25803180cd
agmkowalczyk <agm.kowalczyk@gmail.com>
2017-12-08 02:29:46 +0100

    kodilla 19.3

---
+++ actions.js
@@ -0,0 +1,55 @@
+import uuid from 'uuid';
+
+const ADD_COMMENT = 'ADD_COMMENT';
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid.v4()
+    }
+}
+
+function removeComment(id) {
+    return {
+        type: ADD_COMMENT,
+        id
+    }
+}
+
+function editComment(id, text) {
+    return {
+        type: EDIT_COMMENT,
+        id,
+        text
+    }
+}
+
+function thumbUpComment(id) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id,
+        voted: votes++
+    }
+}
+
+function thumbDownComment(id) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id,
+        voted: votes--
+    }
+}
+
+const boundAddComment = text => dispatch(addComment(text));
+const boundRemoveComment = text => dispatch(removeComment(id));
+const boundEditComment = text => dispatch(editComment(id, text));
+const boundThumbUpComment = text => dispatch(ThumbUpComment(id));
+const boundThumbDownComment = text => dispatch(ThumbDownComment(id));
+
+// boundAddComment('nowy komentarz!');
+// boundAddComment('kolejny nowy komentarz!');
